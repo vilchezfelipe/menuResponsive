887c725b5c5c6447296ddfa12c30e8477fed130f
Felipe <felipemarket.vilchez@outlook.com>
2021-01-25 16:00:58 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+function menu(){
+    let menu = document.getElementById("menu");
+    menu.classList.add("active");
+}
+function cerrar(){
+    let menu = document.getElementById("menu");
+    menu.classList.remove("active");
+}
